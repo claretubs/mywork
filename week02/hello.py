dce3be4e3ee54a8928b97162044480307cfe1bbe
Clare Tubridy <G00354146@atu.ie>
2023-02-02 15:31:22 +0000

    Week 2 task - My Name

--- week02/hello.py
+++ week02/hello.py
@@ -1,2 +1,7 @@
+#hello.py
+#Author: Clare Tubridy
+#Date: 02/02/2023
+#Program that takes an input of your name and displays Hello "inserted name" as an output
+
 name = input('What is your name? ')
 print(f"Hello {name}")
