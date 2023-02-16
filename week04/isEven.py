7f6e299521b9fd70ab46d773efea7366dfc3ae00
Clare Tubridy <G00354146@atu.ie>
2023-02-16 18:28:11 +0000

    Week 04 Lab

--- week04/isEven.py
+++ week04/isEven.py
@@ -2,9 +2,18 @@
 #Author: Clare Tubridy
 #Date: 16/02/2023
 
-number = int(input("enter a number: "))
+number = 0
 
-if (number % 2) == 0:
-    print (f"{number} is an even number")
+
+while True:
+    number = int(input("enter a number: "))
+
+    if (number % 2) == 0:
+        print (f"{number} is an even number")
+    else:
+        print(f"{number} is an odd number")
+    
 else:
-    print(f"{number} is an odd number")
+    print (f"Session is over")
+
+
